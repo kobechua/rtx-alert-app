0647c2c71a3225c48ec907fbfd5b7bff5b8ebc8a
Kobe <kobechua14@gmail.com>
2024-01-23 18:13:03 -0500

    input validation for passowrd, cleaned up code

--- lib/pages/sign_up_page.dart
+++ lib/pages/sign_up_page.dart
@@ -31,23 +31,28 @@ class _SignUpPageState extends State<SignUpPage> {
   @override
   Widget build(BuildContext context) {
 
-    final GlobalKey<FormState> form = GlobalKey<FormState>();
     final FirebaseAuthService auth = FirebaseAuthService();
-
 
     void signUp() async {
       String email = emailController.text;
       String password = passwordController.text;
       String confirmPassword = confirmPasswordController.text;
 
-      if (password != confirmPassword){
-        debugPrint("Passwords are different");
+      if (password.isEmpty){
+          ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text("Password cannot be empty.")));
+      }
+      else if (password != confirmPassword){
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text("Passwords do not match.")));
       }
       else{
         User? user = await auth.signUpWithEmailAndPassword(email, password);
 
         if (!context.mounted) return;
         if (user != null){
+          ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text("Success.")));
           Navigator.push(context, MaterialPageRoute(builder: (context) => const HomePage()));
         }
       }
@@ -55,124 +60,115 @@ class _SignUpPageState extends State<SignUpPage> {
 
 
     return Scaffold(
-      body: Form(
-        key: form,
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text("RTX Alert App", style: TextStyle(
-                color: Colors.black, 
-                fontSize:28.0, 
-                fontWeight: FontWeight.bold,
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text("RTX Alert App", style: TextStyle(
+              color: Colors.black, 
+              fontSize:28.0, 
+              fontWeight: FontWeight.bold,
+              )
+            ),
+                
+                
+            const Text(
+              "Sign Up Page",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 44.0,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+                
+            //Email Field
+            const SizedBox(        
+              height: 34.0
+            ),
+                
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                controller: emailController,
+                keyboardType:  TextInputType.emailAddress,
+                decoration: const InputDecoration(
+                  hintText: "Email",
+                  prefixIcon: Icon(Icons.mail, color: Colors.black)
                 )
               ),
-                  
-                  
-              const Text(
-                "Sign Up Page",
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 44.0,
-                  fontWeight: FontWeight.bold
-                ),
-              ),
-                  
-              //Email Field
-              const SizedBox(        
-                height: 34.0
-              ),
-                  
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  controller: emailController,
-                  keyboardType:  TextInputType.emailAddress,
-                  decoration: const InputDecoration(
-                    hintText: "Email",
-                    prefixIcon: Icon(Icons.mail, color: Colors.black)
-                  )
-                ),
-              ),
-                  
-              //Password Field
-              const SizedBox(
-                height: 10.0
-              ),
-            
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  // validator: (val) {
-                  //   if (val!.isEmpty) return 'Empty';
-                  // },
-                  controller: passwordController,
-                  obscureText: true,
-                  decoration: const InputDecoration(
-                    hintText: "Password",
-                    prefixIcon: Icon(Icons.password, color: Colors.black)
-                  )
+            ),
+                
+            //Password Field
+            const SizedBox(
+              height: 10.0
+            ),
+          
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+
+                controller: passwordController,
+                obscureText: true,
+                decoration: const InputDecoration(
+                  hintText: "Password",
+                  prefixIcon: Icon(Icons.password, color: Colors.black)
                 )
-              ),
-              //Confirm Password Field
-              const SizedBox(
-                height: 10.0
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  controller: confirmPasswordController,
-                  // validator: (val) {
-                  //   if (val!.isEmpty) return 'Empty';
-                  //   if (val!= passwordController.text) return 'No Match';
-                  // },
-                  obscureText: true,
-                  decoration: const InputDecoration(
-                    hintText: "Confirm Password",
-                    prefixIcon: Icon(Icons.password, color: Colors.black)
-                  )
+              )
+            ),
+            //Confirm Password Field
+            const SizedBox(
+              height: 10.0
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                controller: confirmPasswordController,
+                obscureText: true,
+                decoration: const InputDecoration(
+                  hintText: "Confirm Password",
+                  prefixIcon: Icon(Icons.password, color: Colors.black)
                 )
-              ),
+              )
+            ),
+          
+          
+            const SizedBox(
+              height: 17.0,
+            ),
+          
+            // log in button
+            MyButton(
+              onTap: signUp,
+              text: 'Create Account',
+            ),
+                
+            const SizedBox(
+              height: 17.0,
+            ),
             
-            
-              const SizedBox(
-                height: 17.0,
-              ),
-            
-              // log in button
-              MyButton(
-                onTap: signUp,
-                text: 'Create Account',
-              ),
-                  
-              const SizedBox(
-                height: 17.0,
-              ),
-              
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text('Already have an account?'),
-                  const SizedBox(width: 4),
-                  
-                  GestureDetector(
-                    onTap: widget.onTap,
-                    child: const Text(
-                      'Login',
-                      style: TextStyle(
-                        color: Colors.blue, 
-                        fontWeight: FontWeight.bold
-                      ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('Already have an account?'),
+                const SizedBox(width: 4),
+                
+                GestureDetector(
+                  onTap: widget.onTap,
+                  child: const Text(
+                    'Login',
+                    style: TextStyle(
+                      color: Colors.blue, 
+                      fontWeight: FontWeight.bold
                     ),
                   ),
-                ],
-              ),
-            ],
-          ),
+                ),
+              ],
+            ),
+          ],
         ),
-      )
+      ),
     );
   }
 }
