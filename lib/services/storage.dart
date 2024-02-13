5d2d119fb6420981c3a8c18be01a0500cacacbd4
Kobe <kobechua14@gmail.com>
2024-02-13 01:13:56 -0500

    Sends pictures and metadata json to firebase

--- lib/services/storage.dart
+++ lib/services/storage.dart
@@ -1,7 +1,36 @@
 import 'package:firebase_storage/firebase_storage.dart';
+import 'dart:io';
 
-class FirebaseStorageService {
-  FirebaseStorage storage = FirebaseStorage.instance;
+import 'package:path_provider/path_provider.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/widgets.dart';
+
+class Storage {
+  final FirebaseStorage storage = FirebaseStorage.instance;
+  final FirebaseAuth auth = FirebaseAuth.instance;
+
+  late User? user = auth.currentUser;
+
+  Future<void> uploadPhoto(String filePath, String fileName) async {
+    File file = File(filePath);
+
+    if (user == null || user!.email == null) return;
+
+
+    try {
+      DateTime datetime = DateTime.now();
+
+
+      final Directory directory = await getApplicationDocumentsDirectory();
+      final File fileText = File('${directory.path}/myTextFile.json');
+      await fileText.writeAsString('{"month" : ${datetime.month}}, "day" : ${datetime.day}}, "year" : ${datetime.year}}');
+
+      await storage.ref('submissions/${user!.email}/$datetime/photo.jpg').putFile(file);
+      await storage.ref('submissions/${user!.email}/$datetime/metadata.json').putFile(fileText);
+    } catch(e) {
+      debugPrint(e.toString());
+    }
+  }
 
   
 
