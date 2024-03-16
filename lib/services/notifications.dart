81b40e44c76de4c3e492dc0e4765cac0e9516d48
Kobe <kobechua14@gmail.com>
2024-03-16 18:21:13 -0400

    setting up workmanager

--- lib/services/notifications.dart
+++ lib/services/notifications.dart
@@ -1,13 +1,16 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
 
 class FirebaseMessagingAPI{
   final firebaseMessaging = FirebaseMessaging.instance;
+  final database = FirebaseDatabase.instance;
 
-  Future<void> initNotifications() async {
-    await firebaseMessaging.requestPermission();
-    final messagingToken = await firebaseMessaging.getToken();
-    debugPrint(messagingToken);
-    //store to user data in database
-  }
+  // Future<void> initNotifications() async {
+  //   database.ref().child('ActiveUsers/${auth.currentUser!.uid}').set({'sessionID' : convertedSessionID.toString()});
+  //   await firebaseMessaging.requestPermission();
+  //   final messagingToken = await firebaseMessaging.getToken();
+  //   debugPrint(messagingToken);
+  //   //store to user data in database
+  // }
 }
