12d4f5501891ac8f1f2b3b7d208cec31c6989f71
Kobe <kobechua14@gmail.com>
2024-03-18 14:13:33 -0400

    get submission

--- lib/pages/menu/submissions.dart
+++ lib/pages/menu/submissions.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:rtx_alert_app/components/my_button.dart';
 
 class SubmissionPage extends StatefulWidget {
   const SubmissionPage({super.key});
@@ -11,13 +13,30 @@ class SubmissionPage extends StatefulWidget {
 
 class _SubmissionPageState extends State<SubmissionPage> {
 
-  late List<Map<String, dynamic>> badges;
-    final FirebaseAuth auth = FirebaseAuth.instance;
+  late List<Map<String, dynamic>> submissions;
+  final FirebaseAuth auth = FirebaseAuth.instance;
+  User? user = FirebaseAuth.instance.currentUser;
+
+  void getSubmissions() async {
+    final storageRef = FirebaseStorage.instance.ref().child("/submissions/${user!.uid}/");
+    final listResult = await storageRef.listAll();
+    debugPrint("here ${storageRef.name}");
+    debugPrint("here ${listResult.items}");
+    for (var i in listResult.items){
+      debugPrint(i.name);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    getSubmissions();
+    return Scaffold(
 
+
+      body: MyButton(
+        text: "Back",
+        onTap: () {Navigator.pop(context);}
+      ),
     );
   }
 }
