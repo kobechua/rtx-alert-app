e4386d145a7fbc47990630d5ae557795eb7a2666
Kobe <kobechua14@gmail.com>
2024-03-18 15:27:09 -0400

    pulls info from storage

--- lib/pages/menu/submissions.dart
+++ lib/pages/menu/submissions.dart
@@ -13,17 +13,31 @@ class SubmissionPage extends StatefulWidget {
 
 class _SubmissionPageState extends State<SubmissionPage> {
 
-  late List<Map<String, dynamic>> submissions;
+  late Map<String, List<Reference>> submissions;
   final FirebaseAuth auth = FirebaseAuth.instance;
   User? user = FirebaseAuth.instance.currentUser;
 
   void getSubmissions() async {
-    final storageRef = FirebaseStorage.instance.ref().child("/submissions/${user!.uid}/");
-    final listResult = await storageRef.listAll();
-    debugPrint("here ${storageRef.name}");
-    debugPrint("here ${listResult.items}");
-    for (var i in listResult.items){
+    final storage = FirebaseStorage.instance;
+    Map<String, List<Reference>> list = {};
+    final dir =  storage.ref().root.child("/submissions/${user!.uid}/");
+    // final listResult = await dir.root.child("/submissions/${user!.uid}/").listAll();
+    final listResult = await dir.listAll();
+    for (var i in listResult.prefixes){
+      final contents = await i.list();
       debugPrint(i.name);
+      list[i.name] = contents.items;
+    }
+    
+    //print the list
+    debugPrint("User Submissions\n-----------------\n");
+    for (var key in list.keys){
+      debugPrint("\n$key");
+      if (list[key]!.isNotEmpty){
+        for (var ent in list[key]!){
+          debugPrint(ent.fullPath);
+        }
+      }
     }
   }
 
