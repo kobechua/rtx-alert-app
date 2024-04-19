4ed67f898c67e8518e2f63485876c546819bf740
Kobe <kobechua14@gmail.com>
2024-04-19 12:42:51 -0400

    new timer

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 // import 'package:firebase_auth/firebase_auth.dart';
+import 'dart:async';
+
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:rtx_alert_app/firebase_options.dart';
@@ -7,7 +9,7 @@ import 'package:firebase_database/firebase_database.dart';
 // import 'package:geolocator/geolocator.dart';
 import 'package:rtx_alert_app/services/auth.dart';
 import 'package:rtx_alert_app/services/location.dart';
-import 'package:rtx_alert_app/services/session_listener.dart';
+// import 'package:rtx_alert_app/services/session_listener.dart';
 import 'package:workmanager/workmanager.dart';
 import 'package:rtx_alert_app/pages/greeting_page/greeting_page.dart';
 
@@ -75,15 +77,22 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     pingUser();
     // schedulePeriodicTask();
-    return SessionTimeOutListener(
-      duration: const Duration(minutes : 10), onTimeOut: pingUser, onWarning: pingUser,
-        child:
-        ChangeNotifierProvider(
+    // return SessionTimeOutListener(
+    //   duration: const Duration(minutes : 10), onTimeOut: pingUser, onWarning: pingUser,
+    //     child:
+    //     ChangeNotifierProvider(
+    //       create: (context) => AppSettings()..loadSettings(),
+    //       child: const MaterialApp(
+    //         home: GreetingPage(),
+    //       ),
+    //     ), 
+    // );
+    Timer(const Duration(minutes: 2), () {pingUser();});
+    return ChangeNotifierProvider(
           create: (context) => AppSettings()..loadSettings(),
           child: const MaterialApp(
             home: GreetingPage(),
           ),
-        ), 
     );
   }
 }
